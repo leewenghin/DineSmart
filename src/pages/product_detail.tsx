34898dec0f0ebf6fe62edacdb281ee915a68c7e6
Lee Weng Hin <leewenghin1109@e.newera.edu.my>
2023-10-03 19:15:29 +0800

    update prodcut detail page

--- src/pages/product_detail.tsx
+++ src/pages/product_detail.tsx
@@ -1,18 +1,36 @@
-import { faUserGroup } from "@fortawesome/free-solid-svg-icons";
+import {
+  faMinus,
+  faPlus,
+  faUserGroup,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import "./product_detail.css";
 
 function product_detail() {
   return (
     <>
-      <div className="d-flex justify-content-center">
-        <img
-          src="./src/assets/img/beef-bliss.jpg"
-          className="object-fill w-64"
-        />
-      </div>
-      <div>
-        <h1 className="text-primaryColor">Ultimate Beef Bliss Burger</h1>
+      <div className="container mx-auto sm:w-full md:w-3/4 lg:w-1/2 xl:w-1/3 flex items-center flex-col">
+        <div className="object-fill max-w-sm flex flex-col">
+          <img src="./src/assets/img/beef-bliss.jpg" className="object-fill" />
+
+          <div className="name-quantity flex max-w-sm justify-between my-4">
+            <div className="font-medium text-2xl flex-1">
+              Ultimate Beef Bliss Burger
+            </div>
+
+            <div className="quantity flex items-center flex-1 justify-end">
+              <FontAwesomeIcon icon={faMinus} />
+              <div className="border-solid flex items-center border-2 rounded-full h-9 text-center mx-3 px-3 m-auto">
+                1
+              </div>
+
+              <FontAwesomeIcon icon={faPlus} style={{ color: "#eda345" }} />
+            </div>
+          </div>
+
+          <div>Remove from Ultimate Beef Bliss</div>
+        </div>
       </div>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,15 @@ export default {
       }),
       fontFamily: {
       },
+      fontSize: {
+        sm: '0.8rem',
+        base: '1rem',
+        xl: '1.25rem',
+        '2xl': '1.563rem',
+        '3xl': '1.953rem',
+        '4xl': '2.441rem',
+        '5xl': '3.052rem',
+      },
       theme: {
         width: {
           '600': '600px'
