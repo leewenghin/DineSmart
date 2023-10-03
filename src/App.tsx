207c5b2af7fa8f49bf939a90260a60693bb5f549
Lee Weng Hin <leewenghin1109@e.newera.edu.my>
2023-10-03 14:10:56 +0800

    Implementing tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
 import ListGroup from "./components/ListGroup";
 
 function App() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-
-  return (
-    <div>
-      <ListGroup items={items} heading="Cities" />
-    </div>
-  );
+  return <>Hello</>;
 }
 
 export default App;
