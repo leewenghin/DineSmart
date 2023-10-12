c2bc29c8ce64c2d7bfd46d5e136c092292d99fa2
Lee Weng Hin <leewenghin1109@e.newera.edu.my>
2023-10-12 14:33:16 +0800

    Create card payment interface

--- src/pages/card-payment.tsx
+++ src/pages/card-payment.tsx
@@ -2,7 +2,24 @@ function card_payment() {
   return (
     <>
       <div className="full-content">
-        <div></div>
+        <div>
+          <section className="step-wizard">
+            <ul className="step-wizard-list">
+              <li className="step-wizard-item">
+                <span className="progress-count">1</span>
+                <span className="progress-label">Order</span>
+              </li>
+              <li className="step-wizard-item current-item">
+                <span className="progress-count">2</span>
+                <span className="progress-label">Payment</span>
+              </li>
+              <li className="step-wizard-item">
+                <span className="progress-count">3</span>
+                <span className="progress-label">Completed</span>
+              </li>
+            </ul>
+          </section>
+        </div>
       </div>
     </>
   );
